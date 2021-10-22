f67a863d93f6ce74a8b20d63843d9072f2971801
Franz Holzinger <franz.holzinger@festwein.de>
2021-10-22 16:24:13 +0200

    restructuring

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,6 +1,6 @@
 <?php
 
-$EM_CONF[$_EXTKEY] = array(
+$EM_CONF[$_EXTKEY] = [
     'title' => 'Frontend TCE',
     'description' => 'It brings the deprecated TYPO3 4.x class tslib_feTCE and the processScript FEData setup with new features partly back into TYPO3 9+. This is needed by extensions based on the former TYPO3 FORM like tt_board and tt_guest. Only form MAIL and form wizards additionally require the extension compatibility6.',
     'category' => 'plugin',
@@ -10,16 +10,16 @@ $EM_CONF[$_EXTKEY] = array(
     'author' => 'Franz Holzinger',
     'author_email' => 'franz@ttproducts.de',
     'author_company' => 'jambage.com',
-    'constraints' => array(
-        'depends' => array(
-            'php' => '7.0.0-7.4.99',
+    'constraints' => [
+        'depends' => [
+            'php' => '7.2.0-7.4.99',
             'typo3' => '9.5.0-10.5.99',
             'div2007' => '1.10.4-0.0.0',
-        ),
-        'conflicts' => array(
-        ),
-        'suggests' => array(
-        ),
-    ),
-);
+        ],
+        'conflicts' => [
+        ],
+        'suggests' => [
+        ],
+    ],
+];
 
