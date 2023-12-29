2d23082e538df68b3b3009d71910a9e5f206e403
Franz Holzinger <franz.holzinger@festwein.de>
2023-12-29 23:00:50 +0100

    TASK: Run rector rule set LevelSetList::UP_TO_PHP_55

--- Migrations/Code/ClassAliasMap.php
+++ Migrations/Code/ClassAliasMap.php
@@ -3,5 +3,5 @@
 return [
     'tslib_feTCE' => \JambageCom\TslibFetce\Controller\TypoScriptFrontendDataController::class,
     'tslib_content_Form' => \JambageCom\TslibFetce\ContentObject\ContentObject\FormContentObject::class,
-    'TYPO3\\CMS\\Frontend\\ContentObject\\FormContentObject' => \JambageCom\TslibFetce\ContentObject\FormContentObject::class,
+    \TYPO3\CMS\Frontend\ContentObject\FormContentObject::class => \JambageCom\TslibFetce\ContentObject\FormContentObject::class,
 ];
