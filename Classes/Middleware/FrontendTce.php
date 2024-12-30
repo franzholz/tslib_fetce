5ae21496f03a01b3a7517001a060c59804d4a85f
Franz Holzinger <franz.holzinger@festwein.de>
2024-12-30 19:17:19 +0100

    workaround for 1666513645

--- Classes/Middleware/FrontendTce.php
+++ Classes/Middleware/FrontendTce.php
@@ -38,14 +38,16 @@ class FrontendTce implements MiddlewareInterface
      */
     public function process(ServerRequestInterface $request, RequestHandlerInterface $handler): ResponseInterface
     {
-        $tsfetce = GeneralUtility::makeInstance(TypoScriptFrontendTceController::class);
-        $tsfetce->setRequest($request);
         $tsfe = $this->getTypoScriptFrontendController();
         $frontendTypoScript = $request->getAttribute('frontend.typoscript');
-        $typoScriptSetupArray = $frontendTypoScript->getSetupArray();
-        $tsfe->config['FEData'] = ($typoScriptSetupArray['FEData'] ?? '');
-        $tsfe->config['FEData.'] = ($typoScriptSetupArray['FEData.'] ?? '');
-        $dataProcessed = $tsfetce->checkDataSubmission($tsfe);
+        if ($frontendTypoScript->hasSetup()) {
+            $typoScriptSetupArray = $frontendTypoScript->getSetupArray();
+            $tsfe->config['FEData'] = ($typoScriptSetupArray['FEData'] ?? '');
+            $tsfe->config['FEData.'] = ($typoScriptSetupArray['FEData.'] ?? '');
+            $tsfetce = GeneralUtility::makeInstance(TypoScriptFrontendTceController::class);
+            $tsfetce->setRequest($request);
+            $dataProcessed = $tsfetce->checkDataSubmission($tsfe);
+        }
         return $handler->handle($request);
     }
 
