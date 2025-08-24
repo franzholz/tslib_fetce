3c008d596e6f277aa4357ff40cd716ad54760b88
Franz Holzinger <franz.holzinger@festwein.de>
2025-08-24 21:57:00 +0200

    remove support for tslib aliases

--- Migrations/Code/LegacyClassesForIde.php
+++ Migrations/Code/LegacyClassesForIde.php
@@ -4,14 +4,6 @@ namespace {
     die('Access denied');
 }
 
-namespace {
-    /**
-     * @deprecated since 6.0, removed since 7.0
-     */
-    class tslib_content_Form extends \JambageCom\TslibFetce\ContentObject\FormContentObject
-    {
-    }
-}
 
 namespace TYPO3\CMS\Frontend\ContentObject {
     /**
