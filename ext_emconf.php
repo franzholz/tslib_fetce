4f1117a8271738e01c02d4b2d2c63a8bfb168d03
Franz Holzinger <franz.holzinger@festwein.de>
2026-05-13 11:56:19 +0200

    next verion 0.10.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,14 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Frontend TCE',
     'description' => 'It brings the removed TYPO3 4.x class tslib_feTCE and the processScript FEData setup with new features partly back into TYPO3 10 and later. This is needed by extensions based on the former TYPO3 FORM like tt_board and tt_guest.',
     'category' => 'plugin',
-    'version' => '0.10.1',
+    'version' => '0.10.2',
     'state' => 'stable',
     'author' => 'Franz Holzinger',
     'author_email' => 'franz@ttproducts.de',
     'author_company' => 'jambage.com',
     'constraints' => [
         'depends' => [
-            'typo3' => '13.4.0-13.4.99',
+            'typo3' => '13.4.0-14.3.99',
             'div2007' => '2.3.0-0.0.0',
         ],
         'conflicts' => [
